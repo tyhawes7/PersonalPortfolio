6a22fb0785e418469f50f5adacba3200556d0140
tyhawes7 <ty.hawes7@gmail.com>
2022-03-22 16:59:54 -0600

    trying character js

--- sw characters/characters.js
+++ sw characters/characters.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const header = document.querySelector('header')
 const main = document.querySelector('main')
@@ -6,19 +7,33 @@ const main = document.querySelector('main')
 const allCharsButton = document.createElement('button')
 allCharsButton.textContent = 'All Characters'
 allCharsButton.addEventListener('click', function () {
-  console.log('Thanks!')
-  populateDOM()
+  populateDOM(people)
 })
-header.appendChild(allCharsButton)
 
-function populateDOM() {
-  people.forEach((person) => {
+const maleCharacters = people.filter(person => person.gender === 'male') 
+
+const otherCharacters = people.filter((person) => person.gender !== "male" && person.gender !== "female")
+
+const maleCharsButton = document.createElement('button')
+maleCharsButton.textContent = 'Male Characters'
+maleCharsButton.addEventListener('click', () => populateDOM(maleCharacters))
+const otherCharsButton = document.createElement('button')
+otherCharsButton.textContent = 'Other Characters'
+otherCharsButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+header.appendChild(allCharsButton)
+header.appendChild(maleCharsButton)
+header.appendChild(otherCharsButton)
+
+function populateDOM(characters) {
+  removeChildren(main)
+  characters.forEach((person) => {
     const personFig = document.createElement('figure')
     const personImg = document.createElement('img')
     let charNum = getLastNumber(person.url)
     personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
     const personCap = document.createElement('figcaption')
-    personCap.textContent = 'Star wars character'
+    personCap.textContent = person.name
 
     personFig.appendChild(personImg)
     personFig.appendChild(personCap)
@@ -26,7 +41,4 @@ function populateDOM() {
   })
 }
 
-function getLastNumber(url) {
-  const secondToLastLetterOfUrl = url[url.length - 2]
-  return secondToLastLetterOfUrl
-}
+populateDOM(people)
