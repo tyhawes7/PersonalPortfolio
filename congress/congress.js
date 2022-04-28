29c248a05fb914c4fda7fc671881051333636b2f
tyhawes7 <ty.hawes7@gmail.com>
2022-04-28 17:28:27 -0600

    Fixed link

--- congress/congress.js
+++ congress/congress.js
@@ -40,13 +40,38 @@ function simplifiedSenators() {
   
   const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
   
-  const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
+  function simplifiedMembers(people){
+    return people.map((member) => {
+      const middleName = member.middle_name ? ` ${member.middle_name} ` : ` `
+      return {
+        id: member.id,
+        name: `${member.first_name}${middleName}${member.last_name}`,
+        gender: member.gender,
+        party: member.party,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${member.govtrack_id}-200px.jpeg`,
+        seniority: member.seniority,
+        state: member.state,
+        missedVotesPct: member.missed_votes_pct,
+        loyaltyPct: member.votes_with_party_pct
+      }
+    })
+  }
 
-// console.log(biggestMissedVotesPct.missedVotesPct)
 
-// const biggestVacationerList = simplifiedSenators().filter(senator => senator.missedVotesPct === biggestMissedVotesPct.missedVotesPct)
+  if (member.party === 'R'){
+    figCaption.style.setProperty('background-color', '#A60A33')
+   }
+   if (member.party === 'R'){
+     figImg.style.setProperty('border-color', '#A60321')
+    }
 
-// console.log(biggestVacationerList)
+   if (member.party === 'D'){
+     figCaption.style.setProperty('background-color', '#023373')
+   }
+
+   if (member.party === 'D'){
+     figImg.style.setProperty('border-color', '#022859')
+   }
 
 seniorityHeader.textContent = `The most senior Senator is ${mostSeniorMember.name} and the biggest fans of vacations are TBD...`
 
@@ -57,3 +82,30 @@ simplifiedSenators().forEach(senator => {
     loyaltyList.appendChild(listItem)
   }
 })
+
+populateStats(simplifiedMembers(allMembersOfCongress))
+
+  function populateStats(allCongress) {
+    removeChildren(congressStats);
+
+  }
+  const loyaltyLine = document.createElement("p");
+  loyaltyLine.textContent = `The members who have 100% voting loyalty to their parties are:`
+
+  const loyaltyList = document.createElement("ul");
+
+  const biggestMissedVotesPct = allCongress.reduce((acc, member) => acc.missedVotesPct > member.missedVotesPct ? acc : member)
+  
+  const biggestVactionerList = allCongress.filter(member => member.missedVotesPct === biggestMissedVotesPct.missedVotesPct).map(member => member.name).join(' and ')
+  
+  seniorityHeader.textContent = `The most senior member is Don Young and the biggest fan of vacations is ${biggestVactionerList}.`;
+
+  //seniorityHeader.textContent = `The most senior Member is ${mostSeniorMember.name} and the biggest fan of vacations is ${biggestVactionerList}.`
+
+  allCongress.forEach(member => {
+   if(member.loyaltyPct === 100) {
+          let listItem = document.createElement('li')  
+          listItem.textContent = member.name
+          loyaltyList.appendChild(listItem)
+    }
+  })
